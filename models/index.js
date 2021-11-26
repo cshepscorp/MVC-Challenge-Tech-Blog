fb59605b5f7432873ac83f604e49486009a38432
cshepscorp <sheppard.christy@gmail.com>
2021-11-26 14:57:37 -0500

    post route successfully created

--- models/index.js
+++ models/index.js
@@ -1,3 +1,16 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+/* A user can make many posts. But a post only belongs to a single user, and never many users. By this relationship definition, we know we have a one-to-many relationship.*/
+// create associations
+/* This association creates the reference for the id column in the User model to link to the corresponding foreign key pair, which is the user_id in the Post model.*/
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+/* defining the relationship of the Post model to the User. The constraint we impose here is that a post can belong to one user, but not many users. Again, we declare the link to the foreign key, designated at user_id in the Post model */
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
