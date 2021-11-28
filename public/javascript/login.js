166e68fbbac35133533fef4ba45ae8df8ba03181
cshepscorp <sheppard.christy@gmail.com>
2021-11-28 16:41:52 -0500

    login successful

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,18 +1,50 @@
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefault();
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && password) {
-        fetch('/api/users', {
+        const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
             username,
             password
         }),
         headers: { 'Content-Type': 'application/json' }
-        }).then((response) => {console.log(response)})
+        });
+        
+        // check the response status
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
+    }
+  }
+
+  async function loginFormHandler(event) {
+    event.preventDefault();
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch(`/api/users/login`, {
+        method: 'post',
+        body: JSON.stringify({
+            username,
+            password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+        });
+        
+        // check the response status
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
     }
   }
   
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
